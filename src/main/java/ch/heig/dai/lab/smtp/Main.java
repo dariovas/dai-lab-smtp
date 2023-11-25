b5a272e68c098d2d880548c15fc94060585f1512
dariovasq <dario.vasques@heig-vd.ch>
2023-11-25 14:21:46 +0100

    Add number of groups to pass in the argument

--- src/main/java/ch/heig/dai/lab/smtp/Main.java
+++ src/main/java/ch/heig/dai/lab/smtp/Main.java
@@ -2,16 +2,22 @@ package ch.heig.dai.lab.smtp;
 
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws IOException {
+        // Read command line arguments
+        if (args.length != 1) {
+            System.out.println("You need to provide 1 command line arguments: number of groups.");
+            System.exit(1);
+        }
+
+        int nbGroups = Integer.parseInt(args[0]);
 
         SMTPClient client = new SMTPClient();
         List<Group> groups = new ArrayList<Group>();
         Message message = new Message("Meeting2", "Test 122 \n teojj");
-        for(int i = 0; i < 4; ++i){
+        for(int i = 0; i < nbGroups; ++i){
             groups.add(new Group(JsonReader.getVictim()));
             client.send(groups.get(i).getSender(), groups.get(i).getReceiver(), message);
         }
