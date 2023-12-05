cb14062e21a922bb43bb58df620db5a63c0506ac
dariovasq <dario.vasques@heig-vd.ch>
2023-12-05 14:59:41 +0100

    Adds timezone

--- src/main/java/ch/heig/dai/lab/smtp/SMTPClient.java
+++ src/main/java/ch/heig/dai/lab/smtp/SMTPClient.java
@@ -7,7 +7,6 @@ import java.net.Socket;
 import java.nio.charset.StandardCharsets;
 
 import java.time.LocalDateTime;
-import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
 
